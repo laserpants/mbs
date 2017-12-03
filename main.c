90eee6e35392ad3a4674b86a7738eb0cd4134195
Johannes Hilden <hildenjohannes@gmail.com>
2017-12-03 17:03:44 +0300

    Implement command line parsing

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include <ifaddrs.h>
 #include <linux/if_link.h>
+#include <net/if.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,10 +11,13 @@
 #include <unistd.h>
 #include "argtable3.h"
 
-struct context {
-    uint32_t  offset;
-    uint32_t  available;
-    char     *ifa;
+struct state {
+    uint32_t  snapshot;
+    uint64_t  available;
+    uint64_t  used;
+    int       verbose;
+    int       countdown;
+    char     *ifa_name;
 };
 
 struct stats {
@@ -21,8 +25,42 @@ struct stats {
     uint32_t  tx_bytes;
 };
 
+static char *
+get_default_interface ()
+{
+    struct ifaddrs *ifa0, *ifa;
+    char  *ifa_name = NULL;
+
+    if (getifaddrs (&ifa0) == -1)
+    {
+        perror ("getifaddrs");
+        exit (EXIT_FAILURE);
+    }
+
+    for (ifa = ifa0; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        const unsigned short sa_family = ifa->ifa_addr->sa_family;
+        const int ifa_running = ifa->ifa_flags & IFF_RUNNING;
+
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        if (sa_family != AF_INET && sa_family != AF_INET6) 
+            continue;
+
+        if ((strcmp ("lo", ifa->ifa_name) == 0) || !ifa_running)
+            continue;
+
+        ifa_name = strdup (ifa->ifa_name);
+        break;
+    }
+
+    freeifaddrs (ifa0);
+    return ifa_name;
+}
+
 static int 
-poll_interfaces (struct context *ctx, struct stats *stats)
+poll_interfaces (struct state *s, struct stats *stats)
 {
     struct ifaddrs *ifa0, *ifa;
 
@@ -32,13 +70,11 @@ poll_interfaces (struct context *ctx, struct stats *stats)
         exit (EXIT_FAILURE);
     }
 
-    ifa = ifa0;
-
-    while (ifa != NULL)
+    for (ifa = ifa0; ifa != NULL; ifa = ifa->ifa_next)
     {
-        const int family = ifa->ifa_addr->sa_family;
+        const unsigned short int sa_family = ifa->ifa_addr->sa_family;
 
-        if (family == AF_PACKET && 0 == strcmp (ifa->ifa_name, ctx->ifa)) 
+        if (sa_family == AF_PACKET && 0 == strcmp (ifa->ifa_name, s->ifa_name)) 
         {
             const struct rtnl_link_stats *if_stats = ifa->ifa_data;
 
@@ -46,92 +82,213 @@ poll_interfaces (struct context *ctx, struct stats *stats)
             stats->tx_bytes = if_stats->tx_bytes;
 
             freeifaddrs (ifa0);
-
             return 0;
         }
-
-        ifa = ifa->ifa_next;
     }
 
     freeifaddrs (ifa0);
-
     return -1;
 }
 
-struct arg_lit *verb, *help, *version;
-struct arg_end *end;
-
-int 
-main (int argc, char *argv[])
+static uint64_t 
+parse_bytes (const char *str)
 {
+    char *suffix;
+    uint64_t b;
+
+    if (NULL == str || 0 == strlen (str))
+        return 0;
+
+    suffix = (char *) str;
+
+    while (*suffix >= '0' && *suffix <= '9')
+        ++suffix;
+
+    b = strtoull (str, &suffix, 10);
+
+    if (0 == strlen (suffix))
+    {
+        return b;
+    }
+    else if (0 == strcmp ("kB", suffix) || 
+             0 == strcmp ("k", suffix))
+    {
+        return 1000 * b;
+    }
+    else if (0 == strcmp ("KB", suffix) || 
+             0 == strcmp ("K", suffix)  || 
+             0 == strcmp ("KiB", suffix))
+    {
+        return b << 10;
+    }
+    else if (0 == strcmp ("mB", suffix) || 
+             0 == strcmp ("m", suffix))
+    {
+        return 1000 * 1000 * b;
+    }
+    else if (0 == strcmp ("MB", suffix) || 
+             0 == strcmp ("M", suffix)  ||
+             0 == strcmp ("MiB", suffix))
+    {
+        return b << 20;
+    }
+    else if (0 == strcmp ("gB", suffix) || 
+             0 == strcmp ("g", suffix))
+    {
+        return 1000 * 1000 * 1000 * b;
+    }
+    else if (0 == strcmp ("GB", suffix) || 
+             0 == strcmp ("G", suffix)  ||
+             0 == strcmp ("GiB", suffix))
+    {
+        return b << 30;
+    }
+
+    fprintf (stderr, "Unrecognized suffix: %s", suffix);
+    exit (EXIT_FAILURE);
+}
+
+static void 
+parse_args (int argc, char *argv[], struct state *s)
+{
+    struct arg_lit *verb, 
+                   *help, 
+                   *version;
+    struct arg_str *iface;
+    struct arg_end *end;
+    struct arg_str *available;
+
+    int nerrors;
+    const char command[] = "main";
+
     void *argtable[] = {
-        help    = arg_litn(NULL, "help", 0, 1, "display this help and exit"),
-        version = arg_litn(NULL, "version", 0, 1, "display version info and exit"),
-        verb    = arg_litn("v", "verbose", 0, 1, "verbose output"),
-        end     = arg_end(20),
+        help = arg_litn (
+            NULL, "help", 
+            0, 1, "display this help and exit"
+        ),
+        version = arg_litn (
+            NULL, "version", 
+            0, 1, "display version info and exit"
+        ),
+        verb = arg_litn (
+            "v", "verbose", 
+            0, 1, "verbose output"
+        ),
+        available = arg_strn (
+            "b", "available", "<bytes>",
+            0, 1, "bytes available to use in your subscription plan"
+        ),
+        iface = arg_strn (
+            NULL, NULL, "<interface>", 
+            0, 1, "network interface"
+        ),
+        end = arg_end (20),
     };
 
-    int exitcode = 0;
-    char command[] = "main";
-    
-    int nerrors;
-    nerrors = arg_parse (argc,argv,argtable);
+    nerrors = arg_parse (argc, argv, argtable);
 
     if (help->count > 0)
     {
-        printf("Usage: %s", command);
-        arg_print_syntax(stdout, argtable, "\n");
-        printf("Demonstrate command-line parsing in argtable3.\n\n");
-        arg_print_glossary(stdout, argtable, "  %-25s %s\n");
-        exitcode = 0;
-        arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
-        return exitcode;
+        printf ("Usage: %s", command);
+        arg_print_syntax (stdout, argtable, "\n");
+        printf ("Demonstrate command-line parsing in argtable3.\n\n");
+        arg_print_glossary (stdout, argtable, "  %-25s %s\n");
+        arg_freetable (argtable, sizeof (argtable) / sizeof (argtable[0]));
+        exit (EXIT_SUCCESS);
     }
 
     if (nerrors > 0)
     {
-        arg_print_errors(stdout, end, command);
-        printf("Try '%s --help' for more information.\n", command);
-        exitcode = 1;
-        arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
-        return exitcode;
+        arg_print_errors (stdout, end, command);
+        printf ("Try '%s --help' for more information.\n", command);
+        arg_freetable (argtable, sizeof (argtable) / sizeof (argtable[0]));
+        exit (EXIT_FAILURE);
     }
 
-    /* -- */
+    s->ifa_name = strlen (*iface->sval) ? strdup (*iface->sval) 
+                                        : get_default_interface ();
+    s->verbose = !!verb->count;
+    s->available = parse_bytes (*available->sval); 
+    s->countdown = !!available->count;
 
-    struct context ctx;
+    if (s->verbose) 
+    {
+        if (s->countdown)
+            printf (
+                "Running in countdown mode. Available bytes: %lu\n", 
+                s->available
+            ); 
+        else
+            printf ("Not using countdown mode.\n"); 
+    }
+
+    arg_freetable (argtable, sizeof(argtable) / sizeof(argtable[0]));
+}
+
+int 
+main (int argc, char *argv[])
+{
+    struct state state = {
+        0,   /* snapshot */
+        0,   /* available */
+        0,   /* used */
+        0,   /* verbose */
+        0,   /* countdown */
+        NULL /* ifa_name */
+    };
+
     struct stats stats = { 0, 0, NULL };
 
-    ctx.offset = 0;
-    ctx.available = 8000;
+    parse_args (argc, argv, &state);
 
-    ctx.ifa = strdup ("wlp2s0");
-
-    if (-1 == poll_interfaces (&ctx, &stats))
+    if (-1 == poll_interfaces (&state, &stats))
     {
-        fprintf (stderr, "No such interface: %s", ctx.ifa);
+        fprintf (stderr, "No such interface: %s\n", state.ifa_name);
+        exit (EXIT_FAILURE);
     } 
 
-    ctx.offset = stats.tx_bytes + stats.rx_bytes;
+    state.snapshot = stats.tx_bytes + stats.rx_bytes;
+
+    if (state.verbose)
+        printf ("Using network interface %s.\n", state.ifa_name);
 
     while (1)
     {
-        if (-1 == poll_interfaces (&ctx, &stats))
+        if (-1 == poll_interfaces (&state, &stats))
         {
-            fprintf (stderr, "No such interface: %s", ctx.ifa);
+            fprintf (stderr, "No such interface: %s", state.ifa_name);
         } 
         else
         {
-            uint32_t tot = stats.tx_bytes + stats.rx_bytes;
-            uint32_t used = tot - ctx.offset;
-            uint32_t left = ctx.available - used;
+            const uint32_t txrx = stats.tx_bytes + stats.rx_bytes;
+            const uint32_t diff = txrx - state.snapshot;
 
-            printf ("Used: %u\n", used);
-            printf ("Left: %u\n", left);
+            state.used += diff;
+
+            if (state.countdown)
+            {
+                if (state.available > diff)
+                    state.available -= diff;
+                else
+                    state.available = 0;
+            }
+
+            state.snapshot = txrx;
+
+            printf ("Used      : %lu\n", state.used);
+            printf ("Available : %lu\n", state.available);
+
+            if (state.countdown && !state.available)
+            {
+                printf ("Data limit exceeded.\n");
+                break;
+            }
         }
 
         usleep (100000);
     }
+
+    free (state.ifa_name);
 
     return 0;
 }
