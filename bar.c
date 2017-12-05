c26216e2f58b4a41f60c0f24912a7592f275f753
Johannes Hilden <hildenjohannes@gmail.com>
2017-12-05 03:20:44 +0300

    Add bold text

--- bar.c
+++ bar.c
@@ -7,48 +7,37 @@
 void
 bar_draw_box (void)
 {
-    int i, j;
+    int i;
 
-    printf (CSI "2K"); /* Clear line */
-
-    printf("\u250C"); 
-
+    printf("\u250C");
     for (i = 0; i < 79; ++i)
-        printf("\u2500"); 
+        printf("\u2500");
+    printf("\u2510");
 
-    printf("\u2510"); 
+    printf (CSI "79D");      /* Move left */
+    printf(" \033[1mstash\033[22m ");
 
-    printf (CSI "44D"); 
-    printf(" stash "); 
-
-    for (j = 0; j < 2; ++j)
+    for (i = 0; i < 3; ++i)
     {
-        printf (CSI "1E"); /* Move cursor to beginning of next line */
-        printf (CSI "2K"); /* Clear line */
-
-        printf("\u2502"); 
-
-        for (i = 0; i < 79; ++i)
-            printf("  "); 
-
-        printf("\u2502"); 
+        printf (CSI "1E");   /* Move cursor to beginning of next line */
+        printf (CSI "2K");   /* Clear line */
+        printf("\u2502");
+        printf (CSI "81G");  /* Set column */
+        printf("\u2502");
     }
 
-    printf (CSI "1E"); /* Move cursor to beginning of next line */
-    printf (CSI "2K"); /* Clear line */
+    printf (CSI "1E");       /* Move cursor to beginning of next line */
 
-    printf("\u2514"); 
-
+    printf("\u2514");
     for (i = 0; i < 79; ++i)
-        printf("\u2500"); 
-
+        printf("\u2500");
     printf("\u2518"); 
 
-    printf (CSI "2F"); 
+    printf (CSI "3F"); 
 }
 
 void 
-bar_render (struct stash *s)
+bar_render (struct stash *s, bool tx, bool rx)
 {
     int i;
     double tot = s->balance + s->used.tx_bytes + s->used.rx_bytes, 
@@ -65,67 +54,71 @@ bar_render (struct stash *s)
     printf (CSI "2C"); /* Move right */
     printf (CSI "0K"); /* Clear line */
     
-    printf (
-        "Left: %s", 
-        available_str
-    );
-
     printf (CSI "18G"); /* Set column */
 
-    //putchar ('|');
+    if (!(s->flags & FLAG_ASCII))
+        printf ("\u2605 ");
+    printf ("\033[1m%s\033[22m", s->ifa_name);
 
-    for (i = 0; i < 62 * r - 1; ++i)
-        printf ("\u25AE");
-        //printf ("⚫");
-        //printf ("\u25CF");
-        //printf ("\u25C2");
-        //putchar ('=');
+    printf (CSI "34G"); /* Set column */
 
-    //if (i > 0)
-    //{
-    //    printf (CSI "1D"); /* Move left */
-    //    //printf ("\u00B7");
-    //    //putchar ('o');
-    //}
-
-    if (r > 0)
-    {
-        for (; i < 62 - 1; ++i)
-            printf ("\u25AF");
-            //printf ("⚪");
-            //printf ("\u25CB");
-            //printf ("\u25C3");
-            //putchar (' ');
-    }
+    if (true == tx)
+        printf (s->flags & FLAG_ASCII ? "* " : "\033[1m\u2022\033[22m ");
     else
-    {
-        printf ("\u2716");
-        for (i = 0; i < 60; ++i)
-            printf (" ");
-    }
+        printf ("  ");
 
-    printf ("  \u2502");
+    if (!(s->flags & FLAG_ASCII))
+        printf ("\u2196 ");
+
+    printf ("TX: \033[1m%s\033[22m", used_tx_str);
+
+    printf (CSI "50G"); /* Set column */
+
+    if (true == rx)
+        printf (s->flags & FLAG_ASCII ? "* " : "\033[1m\u2022\033[22m ");
+    else
+        printf ("  ");
+
+    if (!(s->flags & FLAG_ASCII))
+        printf ("\u2199 ");
+
+    printf ("RX: \033[1m%s\033[22m", used_rx_str);
+
+    printf (CSI "66G"); /* Set column */
+    printf ("Ctrl+C to exit \u2502");
 
     printf (CSI "1E"); /* Move cursor to beginning of next line */
     printf (CSI "2C"); /* Move right */
     printf (CSI "0K"); /* Clear line */
 
-    printf (
-        "Used: %s", 
-        used_str
-    );
-
-    printf (CSI "18G"); /* Set column */
-    printf ("TX: %s", used_tx_str); 
-
-    printf (CSI "38G"); /* Set column */
-    printf ("RX: %s", used_rx_str); 
-
-    printf (CSI "59G"); /* Set column */
-    printf ("Press Ctrl+C to exit  \u2502");
+    printf ("Used: \033[1m%s\033[22m", used_str);
+    printf (CSI "81G"); /* Set column */
+    printf ("\u2502");
 
     printf (CSI "1E"); /* Move cursor to beginning of next line */
     printf (CSI "2C"); /* Move right */
+    printf (CSI "0K"); /* Clear line */
+
+    printf ("Left: \033[1m%s\033[22m", available_str);
+
+    printf (CSI "18G"); /* Set column */
+
+    if (s->flags & FLAG_ASCII)
+    {
+        printf ("[");
+        for (i = 0; i < 61 * r - 1; ++i)
+            printf ("=");
+        printf (CSI "79G"); /* Set column */
+        printf ("] \u2502");
+    }
+    else
+    {
+        for (i = 0; i < 63 * r - 1; ++i)
+            printf ("\u2588");
+        for (; i < 63 - 1; ++i)
+            printf ("\u2591");
+        printf (" \u2502");
+    }
 
     printf (CSI "2F"); /* Move cursor to beginning of {n} lines up */
 
--- bar.h
+++ bar.h
@@ -2,9 +2,10 @@
 #define BAR_H
 
 #include "stash.h"
+#include <stdbool.h>
 
 void bar_draw_box (void);
-void bar_render (struct stash *s);
+void bar_render (struct stash *s, bool tx, bool rx);
 void bar_restore_term (void);
 
 #endif 
